fb0ea272b53de9f82d1128fbb0fc8069290a8959
nwesnet <sss22872@gmail.com>
2024-09-28 20:04:29 +0200

    Add main page GUI elements

--- src/main/java/pmproject/passwordmanager/LoginController.java
+++ src/main/java/pmproject/passwordmanager/LoginController.java
@@ -1,9 +1,16 @@
 package pmproject.passwordmanager;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+
+import java.io.IOException;
 
 public class LoginController {
     @FXML
@@ -19,8 +26,29 @@ public class LoginController {
     private Button createAccountButton;
 
     @FXML
-    protected void onLoginButtonClick(){
+    protected void onLoginButtonClick(ActionEvent event){
         System.out.printf("Username: %s\nPassword: %s",usernameField.getText(),passwordField.getText());
+        //Close the current login window and open the main window
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main-view.fxml"));
+            Scene mainScene = new Scene(fxmlLoader.load(),650,550);
+
+            //Get the current stage (login window) and close it
+            Stage loginStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            loginStage.close();
+
+            //Open the main window in a new stage
+            Stage mainStage = new Stage();
+            mainStage.setTitle("Password manager");
+            mainStage.setScene(mainScene);
+            mainStage.setMinHeight(550);
+            mainStage.setMinWidth(650);
+            mainStage.show();
+
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
     }
     @FXML
     protected void onCreateAccountButtonClick(){
---
+++ src/main/java/pmproject/passwordmanager/MainPageController.java
@@ -0,0 +1,109 @@
+package pmproject.passwordmanager;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.animation.TranslateTransition;
+import javafx.util.Duration;
+
+public class MainPageController {
+    @FXML
+    private ScrollPane logsScrollPane;
+    @FXML
+    private ScrollPane accountsScrollPane;
+    @FXML
+    private HBox leftHBox;
+    @FXML
+    private VBox accountsVBox;
+    @FXML
+    private HBox bottomHBox;
+    @FXML
+    private Button logsButton;
+    @FXML
+    private Button addAccountButton;
+    @FXML
+    private Button generatePasswordButton;
+    @FXML
+    private Button showAccountsButton;
+    @FXML
+    private TextArea logsTextArea;
+
+    private boolean logsVisible = false;
+    private boolean accountsVisible = false;
+
+    @FXML
+    protected void initialize () {
+        leftHBox.setSpacing(-logsScrollPane.getPrefWidth());
+    }
+
+    @FXML
+    protected void onAddAccountClicked(){
+        // Logic for adding an account
+        System.out.println("Add Account clicked");
+    }
+    @FXML
+    protected void onGeneratePasswordClicked() {
+        // Logic for generating a password
+        System.out.println("Generate Password clicked");
+    }
+
+    @FXML
+    protected void onShowAccountsClicked() {
+        // Logic for showing accounts
+        System.out.println("Show Accounts clicked");
+        accountsVisible = !accountsVisible;
+        if(accountsVisible){
+            accountsScrollPane.setVisible(true);
+            TranslateTransition tt1 = new TranslateTransition(Duration.millis(300),accountsScrollPane);
+            tt1.setFromX(accountsScrollPane.getPrefWidth());
+            tt1.setToX(0);
+            tt1.play();
+        }
+        else {
+            TranslateTransition tt1 = new TranslateTransition(Duration.millis(300),accountsScrollPane);
+            tt1.setFromX(0);
+            tt1.setToX(accountsScrollPane.getPrefWidth());
+            tt1.play();
+            tt1.setOnFinished(event -> {
+                accountsScrollPane.setVisible(false);
+            });
+        }
+    }
+
+    @FXML
+    protected void onLogsClicked() {
+        // Logic for displaying logs
+        System.out.println("Logs clicked");
+        System.out.println(logsVisible);
+        // Toggle visibility of logs pane
+        logsVisible = !logsVisible;
+        if(logsVisible){
+            logsScrollPane.setVisible(true);
+            TranslateTransition tt1 = new TranslateTransition(Duration.millis(300),logsScrollPane);
+            tt1.setFromX(-logsScrollPane.getPrefWidth());
+            tt1.setToX(0);
+            TranslateTransition tt2 = new TranslateTransition(Duration.millis(300),logsButton);
+            tt2.setFromX(0);
+            tt2.setToX(150);
+            tt1.play();
+            tt2.play();
+        }
+        else {
+            TranslateTransition tt1 = new TranslateTransition(Duration.millis(300),logsScrollPane);
+            tt1.setFromX(0);
+            tt1.setToX(-logsScrollPane.getPrefWidth());
+            TranslateTransition tt2 = new TranslateTransition(Duration.millis(300),logsButton);
+            tt2.setFromX(logsScrollPane.getPrefWidth());
+            tt2.setToX(0);
+            tt1.play();
+            tt2.play();
+            tt1.setOnFinished(event -> {
+                logsScrollPane.setVisible(false);
+            });
+        }
+    }
+}
